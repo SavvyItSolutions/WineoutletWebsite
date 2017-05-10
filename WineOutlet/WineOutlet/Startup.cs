6e76b83db7dd6ebdbfe7a78fe95d4066ececef8d
sailokeshgoud <sailokeshgoud@gmail.com>
2017-05-10 12:35:12 +0530

    First Commit

---
+++ WineOutlet/WineOutlet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WineOutlet.Startup))]
+namespace WineOutlet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
